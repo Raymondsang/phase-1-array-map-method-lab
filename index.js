efdababd93ef444523ca88408418173f8f606d32
RAYMONDSANG <raysang98@gmail.com>
2024-07-20 23:12:59 +0300

    first commit

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+      .join(' ');
+  });
 }
+
